c4cabdbc4465d80660437d8eb758812199fd124b
romanLipatov <roman.lipatov@techiosoft.com>
2025-09-23 09:29:29 +0300

    Solution

--- tasks/task2.js
+++ tasks/task2.js
@@ -1,11 +1,20 @@
-// Задача: Написати функцію, яка приймає рядок і повертає його у зворотному порядку,
-//  при цьому пропускаючи всі цифри.
-
 function reverseWithoutNumbers(str) {
-  // Ваш код тут
+  const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (!numbers.includes(char)) {
+      result += char;
+    }
+  }
+  let reversed = "";
+  for (let i = result.length - 1; i >= 0; i--) {
+    reversed += result[i];
+  }
+  return reversed;
 }
 
-console.log(reverseWithoutNumbers("hello123world456")); // Виведе: "dlrowolleh"
-console.log(reverseWithoutNumbers("abc123xyz"));       // Виведе: "zyxabc"
+console.log(reverseWithoutNumbers("hello123world456"));
+console.log(reverseWithoutNumbers("abc123xyz"));
 
 module.exports = reverseWithoutNumbers;
--- tasks/task3.js
+++ tasks/task3.js
@@ -1,11 +1,13 @@
-// Задача: Напишіть функцію sumArray, яка приймає масив чисел і 
-// повертає їхню суму. Використовуйте цикл для обчислення суми.
-
-function sumArray(numbers) {
-  // Ваш код тут
+function sumArray(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    const char = arr[i];
+    sum += arr[i];
+  }
+  return sum;
 }
 
-console.log(sumArray([1, 2, 3, 4, 5])); // Виведе: 15
-console.log(sumArray([10, 20, 30]));    // Виведе: 60
+console.log(sumArray([1, 2, 3, 4, 5]));
+console.log(sumArray([10, 20, 30]));
 
 module.exports = sumArray;
